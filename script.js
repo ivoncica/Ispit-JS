1fef8c69071f6194f5d2cfb6df0b986fccdb6e1f
programer10 <programer10@ucione.local>
2023-03-13 17:11:40 +0100

    grana1

---
+++ script.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    $("#search").keyup(function() {
+        let searchSong = $(this).val();
+        if(searchSong != "") {
+            $.ajax(
+                {
+                    url: "https://itunes.apple.com/search?term="+searchSong,
+                    type: "GET",
+                    dataType:"json",
+                    success: function(data) {
+                        let rezultat= "";
+                        $.each(data.results, function(index, items) {
+                            rezultat += '<li>'+items.artistName+'-'+items.trackName+'</li>';
+                        });
+                        $("#rezultat").html(rezultat);
+                    },
+                    error: function(jqXHR, textStatus, errorThrown) {
+                        let noResult = "";
+                        console.log("AJAX error: "+textStatus+" "+errorThrown)
+                        noResult += "This artist doesn't exist"
+                        $("#rezultat").html(noResult);
+                    }
+                }
+            );
+        } else {
+            $("#rezultat").html("");
+        }
+    }) 
+}) 
+
+
+
+
+
+
+   
